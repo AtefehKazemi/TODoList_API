105e3150dceb7b5de8956d8d120eb5539e30be6b
AtefehKazemi <kazemi9978@gmail.com>
2023-05-10 17:40:30 +0430

    fix signal

--- ToDoList/tasks/signals.py
+++
@@ -1,9 +0,0 @@
-from .models import task
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.utils import timezone
-
-@receiver(pre_save, sender=task)
-def check_values(sender, instance, **kwargs):
-    if instance.due_date <= timezone.now():
-        raise Exception("The Due date must be in the future")
